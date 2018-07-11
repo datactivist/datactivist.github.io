af645fc163f94dc96e3f5d39ba8624402315c51b
Joel Gombin <joel.gombin@gmail.com>
2018-07-11 06:44:54 +0200

    https working for matomo.datactivist.coop, bitch

--- includes/matomo.js
+++ includes/matomo.js
@@ -6,7 +6,7 @@ var _paq = _paq || [];
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//206.189.52.43/";
+    var u="//matomo.datactivist.coop/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
