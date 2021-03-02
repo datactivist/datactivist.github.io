39ad44b1c7c4b7dcde985878c8b57f9456009d6f
CircleCI <circleci@circleci>
2021-03-02 17:03:35 +0000

    [circle ci] deploy

---
+++ js/promise-polyfill.min.5077d8b47b71e13cf266b42902f8dbabcd8ad70a184d6115867b9bafca475073.js
@@ -0,0 +1,1 @@
+'use strict';var promiseUrl=document.querySelector('script[data-id="promise-polyfill"]').getAttribute('data-promise-polyfill-url');/MSIE \d|Trident.*rv:/.test(navigator.userAgent)&&document.write('<script src="'+promiseUrl+'"><\/script>')
